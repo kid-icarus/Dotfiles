97018e5bd2a83d0ac71dd8a52777e2cc2b4765fb
Ryan Kois <ryan.kois@gmail.com>
2022-09-11 22:36:32 -0700

    add lua lsp

--- nvim-lua/cmp-setup.lua
+++ nvim-lua/cmp-setup.lua
@@ -121,3 +121,26 @@ nvim_lsp.tsserver.setup {
   capabilities = capabilities,
   on_attach = on_attach,
 }
+
+nvim_lsp.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
