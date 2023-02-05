ef3ceaee26482f5b778df1a0fed2f2d8656ee376
Ryan Kois <ryan.kois@gmail.com>
2023-02-04 23:19:43 -0800

    use rustup

--- nvim-lua/cmp-setup.lua
+++ nvim-lua/cmp-setup.lua
@@ -163,8 +163,9 @@ nvim_lsp.tsserver.setup {
   },
 }
 
-nvim_lsp.rust_analyzer.setup{
-  on_attach = on_attach
+nvim_lsp.rust_analyzer.setup {
+  on_attach = on_attach,
+  cmd = { 'rustup', 'run', 'stable', 'rust-analyzer' },
 }
 
 nvim_lsp.sumneko_lua.setup {
