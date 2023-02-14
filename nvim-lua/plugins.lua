a3a5680632b3d5109d9234d1d59d469b3fce33b9
Ryan Kois <ryan.kois@gmail.com>
2023-02-13 19:58:24 -0800

    add octo

--- nvim-lua/plugins.lua
+++ nvim-lua/plugins.lua
@@ -57,4 +57,15 @@ return require('packer').startup(function()
     'folke/trouble.nvim',
     requires = 'kyazdani42/nvim-web-devicons',
   }
+  use {
+  'pwntester/octo.nvim',
+  requires = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'kyazdani42/nvim-web-devicons',
+  },
+  config = function ()
+    require"octo".setup()
+  end
+}
 end)
