287f0fab329ea05b5bd95a11a69573fbef24d1b4
Ryan Kois <ryan.kois@gmail.com>
2023-06-29 23:27:41 -0700

    add istanbul ignore snippet

--- nvim-lua/luasnip-snippets.lua
+++ nvim-lua/luasnip-snippets.lua
@@ -6,3 +6,7 @@ local i = ls.insert_node
 ls.add_snippets('go', {
   s('ife', fmt('if {} != nil {{\n\treturn {}\n}}', { i(1, 'err'), i(0) })),
 })
+
+ls.add_snippets('typescript', {
+  s({ trig = 'ise', name = 'istanbul ignore' }, fmt('// istanbul ignore {}\n', i(0, 'next'))),
+})
