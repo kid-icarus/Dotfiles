c491397e599854585b8ba511b068729316e00b96
Ryan Kois <ryan.kois@gmail.com>
2024-05-25 20:24:31 -0700

    jira transitions keymap

--- nvim-config/lua/custom/plugins/jira.lua
+++ nvim-config/lua/custom/plugins/jira.lua
@@ -2,8 +2,12 @@ return {
   dir = '~/Projects/personal/jira.nvim',
   dependencies = {
     'jcdickinson/http.nvim',
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
   },
   config = function()
     require('jira').setup()
+    local t = require 'telescope'
+    vim.keymap.set('n', '<leader>jt', t.extensions.jira.transitions, {})
   end,
 }
